f5c6b5a0269b76501198eecc5bbefd1425c6a5cb
Ryan Gorup <gorup@users.noreply.github.com>
2020-04-30 19:00:40 -0700

    Add ID gen

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-openapi-generator generate -i openapi/v1.yml -g rust-server -o openapi-build
+set -e
+
+java -jar ~/openapi-generator-cli.jar generate -i openapi/v1.yml -g rust-server -o openapi-build
 
 cp openapi-build/src/models.rs src/generatedmodels.rs
 
-sed -i '' '/use models;/d' ./src/generatedmodels.rs
+sed -i '/use models;/d' ./src/generatedmodels.rs
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,20 @@
 #[macro_use]
 extern crate serde;
+use rand::distributions::Alphanumeric;
+use rand::prelude::*;
 
 pub mod generatedmodels;
+
+pub fn new_keyrequest_id() -> String {
+    format!("{}", id(7))
+}
+
+fn id(len: usize) -> String {
+    let mut rng = thread_rng();
+    std::iter::repeat(())
+        .map(|()| rng.sample(Alphanumeric))
+        .filter(|c| c.is_ascii_hexdigit())
+        .map(|c| c.to_ascii_lowercase())
+        .take(len)
+        .collect()
+}
